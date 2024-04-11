879834f670920f87554e4bd84713a978f439ad89
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-04-11 10:38:28 +0900

    init commit

---
+++ app/src/main/java/com/nbcteam3/nbcapplemarket/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nbcteam3.nbcapplemarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
